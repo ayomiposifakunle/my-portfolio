b5aa1399115b249d4cf15706edf593ef8ab2243b
ayomiposifakunle <142907217+ayomiposifakunle@users.noreply.github.com>
2026-02-02 14:39:42 +0100

    Add files via upload

---
+++ src/supabase-client.js
@@ -0,0 +1,6 @@
+// import {createClient} from '@supabase/supabase-js'
+
+// export const supabase = createClient(
+//     "https://nnbymxwurnhrvuekbyih.supabase.co",
+//     "sb_publishable_0sru9ZhANlMSdSQogWFddA_Iag8JrpT"
+// );
